4888a9d05be5a0395b1562d8fbdf5dfce46582a6
Martin Ellis <martin.ellis@gov.scot>
2018-10-22 14:05:13 +0100

    Add usertype to Google Tag Manager (GTM) dataLayer

---
+++ assets/js/usertype.js
@@ -0,0 +1,12 @@
+(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer[0].userType = response.userType;
+        }
+    };
+    xhr.open('GET', 'https://beta.gov.scot/service/usertype', true);
+    xhr.send(null);
+})();
