d4f416afc2c59b4069376798b7fc2df352676903
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2019-08-15 09:44:32 +0100

    MGS-5309 experiment to put GTM code in userType callback function (with fallback timeout)

--- assets/js/usertype.js
+++ assets/js/usertype.js
@@ -1,12 +1,19 @@
 (function() {
     window.dataLayer = window.dataLayer || [];
 
+    var t = window.setTimeout(function () {
+        initGTM();
+    }, 1000);
+
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
             window.dataLayer = window.dataLayer || [];
             window.dataLayer[0].userType = response.userType;
+
+            initGTM();
+            clearTimeout(t);
         }
     };
     xhr.open('GET', '/service/usertype', true);
