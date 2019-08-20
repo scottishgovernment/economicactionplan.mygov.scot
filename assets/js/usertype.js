d55d57d1a2616b70b0eaa38443cd85360b52b51a
Martin Ellis <martin.ellis@gov.scot>
2019-08-20 13:25:44 +0100

    MGS-5438 Fix userType code for Internet Explorer
    The timeout for the userType request must not be set until after the
    call to open(), for compatibility with Internet Explorer.

--- assets/js/usertype.js
+++ assets/js/usertype.js
@@ -1,21 +1,23 @@
 (function() {
-    window.dataLayer = window.dataLayer || [];
+
+    window.dataLayer = window.dataLayer || [{}];
 
     var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/service/usertype', true);
+
     xhr.timeout = 1000;
+
     xhr.onreadystatechange = function () {
-        if(xhr.readyState === 4) {
+        if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 var response = JSON.parse(xhr.responseText);
-                window.dataLayer = window.dataLayer || [];
                 window.dataLayer[0].userType = response.userType;
             }
             initGTM();
         }
     };
-    xhr.ontimeout = function () {
-        initGTM();
-    };
-    xhr.open('GET', '/service/usertype', true);
+
+    xhr.ontimeout = initGTM;
+
     xhr.send(null);
 })();
