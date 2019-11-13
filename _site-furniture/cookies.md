6508ef8a49cefa46f724c602a1da51256977a6a5
Chris Clark <chris.clark@gov.scot>
2019-11-13 15:56:24 +0000

    further update to replacement cookie banner

--- _site-furniture/cookies.md
+++ _site-furniture/cookies.md
@@ -1,86 +1,23 @@
 ---
-layout: basic
+layout: cookie-preferences
 title: Cookies
 permalink: /cookies/
 parent: home
 thispage: cookies
-headings:
- - name: Cookies
- - name: Why we use cookies
- - name: Cookies that we set
-   nested:
-    - name: Google Analytics
-    - name: YouTube
- - name: Further information
 introduction: A cookie lets a website recognise your device and store some information about your preferences or interactions.
 ---
 
 ## Why we use cookies
 We use economicactionplan.mygov.scot cookie tracking data to make informed decisions on whether the site is meeting your needs, which leads to us making improvements. For example, we monitor how much traffic each part of the site gets and work to improve areas which are not being found when they should be.
 
-<div class="info-note note">
+<div class="ds_information-text">
+<span class="ds_information-text__text">
+<span class="visually-hidden  hidden">Information:</span>
 The cookies that we set on economicactionplan.mygov.scot cannot be used to identify you personally. <a href="https://ico.org.uk/your-data-matters/online/cookies/">Find out more about how to manage cookies for all websites</a>
+</span>
 </div>
 
-## Cookies that we set
+## Turn cookies on or off
+We use 3 main types of cookie on our website. You can choose which cookies you're happy for us to use.
 
-We store information about:
-* the pages you visit and how long you spend on each page
-* how you got to mygov.scot
-* what you click on while you're visiting mygov.scot
-
-We do not:
-* collect information that can be used to identify you; for example, we never receive your name or address
-* make any attempt to find out the identities of people visiting mygov.scot
-* allow the tools that we use to do this either
-* sell, trade or give your details to third parties, unless we are required to do it by law
-
-All information we get through cookies and analytics will be treated in confidence.
-
-### Google Analytics
-
-We anonymise the last 3 digits of all economicactionplan.mygov.scot user IP addresses for Google Analytics, so that we can ensure that you are protected ([you can read more about how this works here](https://support.google.com/analytics/answer/2763052?hl=en)). We can only see a general overview of which regions and cities you may come from, not where individuals access from.
-
-[Find out more about security and privacy in Google Analytics.](https://support.google.com/analytics/answer/2838718?hl=en-GB)
-
-You can opt [out of Google analytics](https://tools.google.com/dlpage/gaoptout) cookies.
-
-<table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Information</th>
-            <th>Expiry</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>_ga</td>
-            <td>This is used to understand and differentiate between different users.</td>
-            <td>38 Months</td>
-        </tr>
-        <tr>
-            <td>_gid</td>
-            <td>_gid has a different ID for each page set on, and a timestamp.  This assists with plotting user journeys.</td>
-            <td>24 Hours</td>
-        </tr>
-        <tr>
-            <td>_gat</td>
-            <td>We use Google Tag Manager to tailor our tracking in case we have different elements that are being tested. For example, setting a tag to track how far a user is having to scroll down a long page. The _gat tag pushes the request rate through quicker.</td>
-            <td>1 Minute</td>
-        </tr>
-    </tbody>
-</table>
-
-### YouTube
-Pages on mygov.scot which contain YouTube videos will not set third-party cookies (we use YouTube-nocookie.com &mdash; [you can read more here](https://support.google.com/youtube/answer/171780?hl=en-GB)).
-
-## Further information
-
-You can email the performance team for further information.
-
-Email: [performance@gov.scot](mailto:performance@gov.scot)
-
-<div class="info-note note">
-This is a live document and will continue to be updated on a bi-monthly basis ensuring it complies with industry standards.
-</div>
+[Find out more about the cookies we use](/cookie-details/)
---
+++ assets/scripts/dist/userType.es5.js
@@ -0,0 +1,111 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./assets/scripts/usertype.js":
+/*!************************************!*\
+  !*** ./assets/scripts/usertype.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+    eval("(function () {\n  function setUserType(userType) {\n    window.dataLayer[0].userType = userType;\n  }\n\n  window.dataLayer = window.dataLayer || [{}];\n  var xhr = new XMLHttpRequest();\n  xhr.open('GET', 'https://www.gov.scot/service/usertype', true);\n  xhr.timeout = 1000;\n\n  xhr.onreadystatechange = function () {\n    if (xhr.readyState === 4) {\n      var userType;\n\n      if (xhr.status === 200) {\n        var response = JSON.parse(xhr.responseText);\n        userType = response.userType;\n      } else {\n        userType = 'error';\n      }\n\n      setUserType(userType);\n      initGTM();\n    }\n  };\n\n  xhr.ontimeout = function () {\n    setUserType('timeout');\n    initGTM();\n  };\n\n  xhr.send(null);\n})();\n\n//# sourceURL=webpack:///./assets/scripts/usertype.js?");
+
+    /***/ }),
+    
+    /***/ 1:
+    /*!******************************************!*\
+      !*** multi ./assets/scripts/usertype.js ***!
+      \******************************************/
+    /*! no static exports found */
+    /***/ (function(module, exports, __webpack_require__) {
+    
+    eval("module.exports = __webpack_require__(/*! /economicactionplan.mygov.scot/assets/scripts/usertype.js */\"./assets/scripts/usertype.js\");\n\n\n//# sourceURL=webpack:///multi_./assets/scripts/usertype.js?");
+    
+    /***/ })
+    
+    /******/ });
